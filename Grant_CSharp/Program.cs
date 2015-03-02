a2844eadbf87831ed5966553f0d2c7a630a72140
Grant Crofton <grantac@hotmail.com>
2015-03-02 23:24:29 +0000

    Added ackermann and tree search

--- Grant_CSharp/Program.cs
+++ Grant_CSharp/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Recursion_Solutions_CSharp {
     static class Program {
+
+        /***************************
+         ********** Basic **********
+         ***************************/
 
         static int factorial(int n) {
             return n == 0
@@ -51,13 +56,42 @@ namespace Recursion_Solutions_CSharp {
             }
         }
 
+        /**********************************
+         ********** Intermediate **********
+         **********************************/
+
+        static int ackermann(int m, int n) {
+            if (m == 0)
+                return n + 1;
+            else if (n == 0)
+                return ackermann(m - 1, 1);
+            else
+                return ackermann(m - 1, ackermann(m, n - 1));
+        }
+
+        static bool treesearch(string filename, string path) {
+            var filesFolders = new DirectoryInfo(path).EnumerateFileSystemInfos();
+
+            if (filesFolders.Where(info => info.Name == filename).Count() > 0)
+                return true;
+
+            return filesFolders
+                .Where( info => info is DirectoryInfo )
+                .Any( directory => treesearch(filename, directory.FullName ));
+        }
+
         static void Main(string[] args) {
 
+            Console.WriteLine("Basic\n");
             Console.WriteLine("Factorial 5: {0}", factorial(5));
             Console.WriteLine("Fibonacci 7: {0}", fibonacci(7));
             Console.WriteLine("Sum [1;2;3;4;5] = {0}", sum(new List<int> {1,2,3,4,5}));
             Console.WriteLine("Count 1 [1,2,1,3,4,1] = {0}", count(1, new List<int> { 1, 2, 1, 3, 4, 1 }));
             Console.WriteLine("Evens [1,2,3,4,5,6,7,8] = {0}", evens(new LinkedList<int>(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 })).print());
+
+            Console.WriteLine("\nIntermediate\n");
+            Console.WriteLine("Ackermann 3 10: {0}", ackermann(3,10));
+            Console.WriteLine("treesearch c:temp {0}", treesearch("thefile.txt", @"c:\temp"));
         }
     }
 }
--- Grant_Scheme/Recursion_Solutions_Scheme.rkt
+++ Grant_Scheme/Recursion_Solutions_Scheme.rkt
@@ -4,6 +4,10 @@
 (define (head l) (car l))
 (define (tail l) (cdr l))
 
+;*****************
+;***** Basic *****
+;*****************
+
 (define (factorial n)
   (cond 
     ((eq? n 0) 1)
@@ -11,6 +15,14 @@
 
 '(Factorial 5)
 (factorial 5)
+
+(define (gcd p q)
+  (cond 
+    ((= q 0) p)
+    (else (gcd q (modulo p q)))))
+
+'(gcd 102 68)
+(gcd 102 68)
 
 (define (fibonacci n)
   (cond 
@@ -50,3 +62,32 @@
 
 '(evens 1 2 3 4 5 6 7 8)
 (evens '(1 2 3 4 5 6 7 8))
+
+;************************
+;***** Intermediate *****
+;************************
+
+(define (ackermann m n)
+  (cond 
+    ((= m 0) (+ n 1))
+    ((= n 0) (ackermann (- m 1) 1))
+    (else (ackermann (- m 1) (ackermann m (- n 1))))))
+
+'(ackermann )
+;(ackermann 3 12)
+
+(define (atom? x)
+  (and (not (pair? x)) (not (null? x))))
+
+(define (treesearch item list)
+  (cond 
+    ((null? list) #f)
+    ((atom? (head list)) (or (eq? (head list) item)
+                             (treesearch item (cdr list))))
+    (else (or (treesearch item (car list))
+              (treesearch item (cdr list))))))
+
+'(treesearch)
+(treesearch 9 '(1 2 (3) (4 5 (6 (7)) 8 ((9)))))
+(treesearch 9 '(((9)) 2 (3) (4 5 (6 (7)) 8)))
+(treesearch 9 '(1 2 (3) (4 5 (6 (7)) 8 (()))))
